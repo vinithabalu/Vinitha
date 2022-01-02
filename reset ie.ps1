a62e84319e1da78605abc1a489a0e9416543d808
Vinitha B <55078448+vinithabalu@users.noreply.github.com>
2022-01-02 18:46:12 +0530

    Reset IE settings.

---
+++ reset ie.ps1
@@ -0,0 +1,8 @@
+﻿ get-item 'HKCU:\Software\Microsoft\Internet Explorer\settings' 
+ get-childitem 'HKCU:\Software\Microsoft\Internet Explorer'  
+ Write-Host  "Resetting IE To Default Settings" -ForegroundColor 'Red'   
+ write-host "No IE process are currently running"  -ForegroundColor 'Green'   
+ Write-Host "Reset button" -ForegroundColor 'Yellow'    
+ & RunDll32.exe InetCpl.cpl,ResetIEtoDefaults  
+ Write-Host "Launch Internet Explorer Now" -ForegroundColor 'Magenta'   
+ Write-Host  "Resetting Done" -ForegroundColor 'Red'                 
